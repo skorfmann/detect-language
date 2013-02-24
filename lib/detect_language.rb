dff28499d72ba06a335db72bef90b9115770acb9
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2013-02-24 12:19:30 +0700

    Working language detection.

--- lib/detect_language.rb
+++ lib/detect_language.rb
@@ -3,4 +3,34 @@
 require 'babel'
 
 class DetectLanguage
+  LANG_PATH = File.join(File.expand_path(File.dirname(__FILE__)), %w(.. lang))
+
+  def initialize
+    languages.each do |language|
+      learn(language)
+    end
+  end
+
+  def detect(string)
+    string.language
+  end
+
+  def languages
+    @languages ||= language_files('*').map do |language_file|
+      language = File.basename(language_file)
+      language.split('.').first.downcase
+    end.uniq.sort
+  end
+
+  private
+
+  def learn(language)
+    language_files("#{language}.*").each do |file|
+      Babel.learn(language, IO.read(file))
+    end
+  end
+
+  def language_files(pattern)
+    Dir[File.join(LANG_PATH, pattern)]
+  end
 end
--- spec/detect_language_spec.rb
+++ spec/detect_language_spec.rb
@@ -1,8 +1,27 @@
+#encoding: utf-8
 require 'rspec'
 require File.join(File.expand_path(File.dirname(__FILE__)), %w(.. lib detect_language))
 
 describe DetectLanguage do
-  it 'works' do
-    expect(true).to eql(true)
+  subject { DetectLanguage.new }
+
+  its(:languages) { should == %w(english french gaelic german) }
+
+  context 'detect' do
+    it 'knows english' do
+      expect(subject.detect("hello, how are you going?")).to eql('english')
+    end
+
+    it 'knows french' do
+      expect(subject.detect("Ceci pourrait expliquer une guerre dans un contexte")).to eql('french')
+    end
+
+    it 'knows gaelic' do
+      expect(subject.detect("Ar thaobh na Fearsaide, arís")).to eql('gaelic')
+    end
+
+    it 'knows german' do
+      expect(subject.detect("Im August 1939 schlossen Deutschland und die Sowjetunion")).to eql('german')
+    end
   end
 end
